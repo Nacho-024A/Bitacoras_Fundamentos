751156909dca61b0f25b6ef8180d9158f5353f28
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2024-11-06 11:42:27 -0500

    Delete Semanas 11-15/Herencia2/PersonaFisica.java

--- Semanas 11-15/Herencia2/PersonaFisica.java
+++
@@ -1,5 +0,0 @@
-public class PersonaFisica extends Persona {
-    public PersonaFisica(String nombre, String apellido, int edad, String genero, String direccion, String telefono, String documentoIdentidad, String nit) {
-        super(nombre, apellido, edad, genero, direccion, telefono, documentoIdentidad, nit);
-    }
-}
