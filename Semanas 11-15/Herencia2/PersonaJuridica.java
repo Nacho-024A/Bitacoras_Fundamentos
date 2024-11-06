f67d354a571a99e357b51850a53869507c6e5670
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2024-11-06 11:42:38 -0500

    Delete Semanas 11-15/Herencia2/PersonaJuridica.java

--- Semanas 11-15/Herencia2/PersonaJuridica.java
+++
@@ -1,5 +0,0 @@
-public class PersonaJuridica extends Persona {
-    public PersonaJuridica(String nombre, String apellido, int edad, String genero, String direccion, String telefono, String documentoIdentidad, String nit) {
-        super(nombre, apellido, edad, genero, direccion, telefono, documentoIdentidad, nit);
-    }
-}
