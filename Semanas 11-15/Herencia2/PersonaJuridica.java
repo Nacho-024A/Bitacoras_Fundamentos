552feb5f9100adb00c7b10ce89ff238afe2efcc4
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2024-11-06 11:36:54 -0500

    Create PersonaJuridica.java

---
+++ Semanas 11-15/Herencia2/PersonaJuridica.java
@@ -0,0 +1,5 @@
+public class PersonaJuridica extends Persona {
+    public PersonaJuridica(String nombre, String apellido, int edad, String genero, String direccion, String telefono, String documentoIdentidad, String nit) {
+        super(nombre, apellido, edad, genero, direccion, telefono, documentoIdentidad, nit);
+    }
+}
