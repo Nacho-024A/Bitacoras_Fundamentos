88517ff1b1cd5cfcf0d9db3508686f2feb2ef38b
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2024-11-06 11:24:10 -0500

    Create Main.java

---
+++ Semanas 11-15/Herencia/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Crear un circulo
+        Circulo circulo = new Circulo("Azul", 5.0);
+        circulo.imprimir();
+
+        // Crear un rectángulo
+        Rectangulo rectangulo = new Rectangulo("Verde", 4.0, 6.0);
+        rectangulo.imprimir();
+
+    }
+}
